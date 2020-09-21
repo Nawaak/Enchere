63dd8794b36072bba816287424b13badd0eac8ef
Nawaak <seveste.brandon@gmail.com>
2020-09-21 14:20:22 +0200

    feat: vitejs

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+import './css/app.scss';
+import './js/count-time.js'
+
+$('[data-toggle="popover"]').popover({
+    html: true,
+    sanitize: false,
+    content: `<div class="d-flex align-items-around flex-grow-1 w-100">
+        <input type="text" class="form-control form-control-sm" placeholder="Entrer un montant">
+        <a class="btn btn-sm btn-success text-white mx-2" onclick="alert('Êtes-vous sûr?')">Valider</a>
+        <a class="btn btn-sm btn-danger text-white" id="pop-close">Annuler</a>
+</div>`
+})
+$('html').on('click', function(e) {
+    if (typeof $(e.target).data('original-title') == 'undefined' && !$(e.target).parents().is('.popover') || e.target.id === 'pop-close') {
+        $('[data-original-title]').popover('hide');
+    }
+});
+
