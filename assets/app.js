8d6f781f7b12164b40474aad5218c424f98c4ea3
Nawaak <seveste.brandon@gmail.com>
2020-10-02 16:16:58 +0200

    refactor: card annonces

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@ $('[data-toggle="popover"]').popover({
     html: true,
     sanitize: false,
     content: `<div class="d-flex align-items-around flex-grow-1 w-100">
-        <input type="text" class="form-control form-control-sm" placeholder="Entrer un montant">
+        <input type="text" class="form-control form-control-sm" autofocus placeholder="Entrer un montant">
         <a class="btn btn-sm btn-success text-white mx-2" onclick="alert('Êtes-vous sûr?')">Valider</a>
         <a class="btn btn-sm btn-danger text-white" id="pop-close">Annuler</a>
 </div>`
