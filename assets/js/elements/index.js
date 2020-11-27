af04ed5dc2cf18bfd907922344e4c94ce0e2ccef
Nawaak <seveste.brandon@gmail.com>
2020-11-27 21:20:56 +0100

    fix: Fix des envoie de notification en Base feat(js): CustomElement pour temps relatif Filtre twig pour afficher temps relatifs par rapport a une date

--- assets/js/elements/index.js
+++ assets/js/elements/index.js
@@ -1,5 +1,7 @@
 import RemainingTime from "/js/elements/CountTime";
 import OfferCreate from "/js/elements/OfferCreate";
+import {TimeAgo} from "/js/elements/TimeAgo";
 
 customElements.define('count-time',RemainingTime);
 customElements.define('offer-create',OfferCreate);
+customElements.define('time-ago',TimeAgo);
